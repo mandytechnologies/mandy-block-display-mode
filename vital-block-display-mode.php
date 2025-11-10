c4aa07fbb420f047b0c63693fd5f9844e0a7e076
Bhavan Aditya <bhavan@vitaldesign.com>
2025-11-11 00:03:16 +0530

    Fix the update issue by removing unwanted codes

--- vital-block-display-mode.php
+++ vital-block-display-mode.php
@@ -21,16 +21,6 @@ class MandyBlockDisplayMode {
 	public static function setup() {
 		add_action('enqueue_block_editor_assets', [__CLASS__, 'enqueue_block_editor_assets']);
 		add_filter('skeletor_block_data', [__CLASS__, 'update_block_class'], 9, 2);
-
-		if (!class_exists('\Skeletor\Plugin_Updater')) {
-			require_once(__DIR__ . '/class--plugin-updater.php');
-		}
-
-		$updater = new \Skeletor\Plugin_Updater(
-			plugin_basename(__FILE__),
-			MANDY_BLOCK_DISPLAY_MODE_VERSION,
-			'https://github.com/mandytechnologies/mandy-block-display-mode/blob/main/package.json'
-		);
 	}
 
 	/**
